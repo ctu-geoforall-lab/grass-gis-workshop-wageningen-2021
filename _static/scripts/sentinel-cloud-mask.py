f411c198175a11b16d4afe769f1b3eaaede737d7
Martin Landa <landa.martin@gmail.com>
2021-09-06 11:46:27 +0200

    u07 updated

--- _static/scripts/sentinel-cloud-mask.py
+++ _static/scripts/sentinel-cloud-mask.py
@@ -6,34 +6,46 @@
 #%option G_OPT_V_MAP
 #% description: Name of AOI vector map
 #%end
+#%option G_OPT_STRDS_INPUT
+#% description: Name of input 4th band space time raster dataset
+#%end
 #%option G_OPT_F_OUTPUT
 #%end
 
 import sys
 import os
 from datetime import datetime, timedelta
+from subprocess import PIPE
 
 import grass.script as gs
 
 from grass.pygrass.gis import Mapset
 from grass.pygrass.modules import Module
-from grass.pygrass.vector import Vector
+from grass.pygrass.vector import VectorTopo
 from grass.pygrass.utils import copy
+import grass.temporal as tgis
 
 def main():
     mapset = Mapset()
     mapset.current()
 
+    tgis.init()
+    sp4 = tgis.open_old_stds(options['input'], 'raster')
+
     with open(options['output'], 'w') as fd:
-        for rast in mapset.glist('raster', pattern='*_B04_10m'):
-            items = rast.split('_')
-            d = datetime.strptime(items[1], '%Y%m%dT%H%M%S')
+        for t_item in sp4.get_registered_maps(columns='name,start_time'):
+            items = t_item[0].split('_')
+            d = t_item[1]
             ## workaround
             dd = d + timedelta(seconds=1)
 
             vect = '{}_{}_MSK_CLOUDS'.format(items[0], items[1])
             mask_vect = '{}_{}'.format(vect, options['map'].split('@')[0])
-            if Vector(vect).exist():
+            n_clouds = 0
+            with VectorTopo(vect) as v:
+                if v.exist():
+                    n_clouds = v.num_primitive_of('centroid')
+            if n_clouds > 0:
                 Module('v.overlay', ainput=options['map'], binput=vect, operator='not',
                        output=mask_vect, overwrite=True)
             else:
@@ -43,8 +55,8 @@ def main():
             Module('g.rename', raster=['MASK', mask_vect])
             fd.write("{0}|{1}|{2}{3}".format(
                 mask_vect,
-                d.strftime('%Y-%m-%d %H:%M:%S'),
-                dd.strftime('%Y-%m-%d %H:%M:%S'),
+                d.strftime('%Y-%m-%d %H:%M:%S.%f'),
+                dd.strftime('%Y-%m-%d %H:%M:%S.%f'),
                 os.linesep))
         
     return 0
